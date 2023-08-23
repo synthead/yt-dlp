71d89ebbcf6c99863bf81276ab5e929eeef074eb
Maxwell Pray <synthead@gmail.com>
2023-08-23 00:53:49 -0700

    Add YouTube Studio extractor.

--- yt_dlp/extractor/youtube.py
+++ yt_dlp/extractor/youtube.py
@@ -7330,3 +7330,50 @@ class YoutubeTruncatedIDIE(InfoExtractor):
         raise ExtractorError(
             f'Incomplete YouTube ID {video_id}. URL {url} looks truncated.',
             expected=True)
+
+class YoutubeStudioIE(YoutubeBaseInfoExtractor):
+    IE_NAME = 'youtube:studio'
+    IE_DESC = "YouTube Studio"
+    _VALID_URL = r'^https?://studio\.youtube\.com/video/(?P<id>[0-9A-Za-z_-]{11})(/edit)?$'
+
+    def _real_extract(self, url):
+        video_id = self._match_id(url)
+
+        yt_query = {
+            'videoId': video_id,
+            'ttsTrackId': {
+              'kind': 'asr',
+              'lang': 'en'
+            },
+            'preferredFormat': 'vtt'
+        }
+
+        response = self._extract_response(
+            default_client='web_creator',
+            ep='globalization/download_caption_track',
+            item_id=video_id,
+            query=yt_query,
+            note='Downloading subtitles',
+            fatal=True
+        )
+
+        captions = base64.urlsafe_b64decode(response["content"]).decode("utf-8")
+
+        subtitles = {
+            'en': [
+                {
+                  'data': captions,
+                  'automatic_captions': captions,
+                  'ext': 'vtt'
+                }
+            ]
+        }
+
+        formats = [{ 'url': url }]
+
+        return {
+            'formats': formats,
+            'id': video_id,
+            'title': 'captions',
+            'subtitles': subtitles
+        }
